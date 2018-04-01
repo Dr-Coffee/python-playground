3457b52d0a60723a6c10d0d66b8284e8d801df9c
Yuanhao Yu <yhyu.mail@gmail.com>
2018-04-01 15:29:55 -0400

    complete the code

--- cnn_mnist.py
+++ cnn_mnist.py
@@ -113,8 +113,35 @@ def main(unused_argv):
     eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
     # Create the estimator
     mnist_classifier = tf.estimator.Estimator(
-        model_fn=cnn_model_fn, model_dir="/tmp/mnist_convnet_model"
+        model_fn=cnn_model_fn, model_dir="tmp_model/"
     )
+    # Set up logging for predictions
+    tensors_to_log = {"probabilities": "softmax_tensor"}
+    logging_hook = tf.train.LoggingTensorHook(
+        tensors=tensors_to_log, every_n_iter=50
+    )
+    # Train the model
+    train_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={"x": train_data},
+        y= train_labels,
+        batch_size=100,
+        num_epochs=None,
+        shuffle=True
+    )
+    mnist_classifier.train(
+        input_fn=train_input_fn,
+        steps=20000,
+        hooks=[logging_hook]
+    )
+    # Evaluate the model and print results
+    eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={"x": eval_data},
+        y=eval_labels,
+        num_epochs=1,
+        shuffle=False
+    )
+    eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
+    print(eval_results)
 
 if __name__ == "__main__":
     tf.app.run()
