8d705f1d6de483ae0163f5fd328ac9b239279a8c
Yuanhao Yu <yhyu.mail@gmail.com>
2018-04-02 15:39:39 -0400

    temp

--- tf_examples.py
+++ tf_examples.py
@@ -1,14 +1,27 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import tensorflow as tf
+import os
+import numpy as np
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-def train_input_fn(features, labels, batch_size):
-    # Convert the inputs to a Dataset
-    dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
-    # Shuffle, repeat, and batch the examples
-    return dataset.shuffle(1000).repeat().batch(batch_size)
+def learn_single_function():
+    x_data = np.random.rand(100).astype(np.float32)
+    y_data = x_data * 0.1 + 0.3
+    weights = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+    biases = tf.Variable(tf.zeros([1]))
+    y = weights * x_data + biases
+    loss = tf.reduce_mean(tf.square(y - y_data))
+    optimizer = tf.train.GradientDescentOptimizer(0.5)
+    train = optimizer.minimize(loss)
+    init = tf.global_variables_initializer()
+    # principles above
+    sess = tf.Session()
+    sess.run(init)
+    for step in range(150):
+        sess.run(train)
+        if step % 20 == 0:
+            print(step, sess.run(weights), sess.run(biases))
+
+
 
 
 
