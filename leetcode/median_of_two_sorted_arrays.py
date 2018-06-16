070fa0c93415776b2f545d3e2b5603b8284f6767
Yuanhao Yu <yhyu.mail@gmail.com>
2018-06-16 17:19:50 -0400

    leetcode 04, have basic idea to slove it

--- leetcode/median_of_two_sorted_arrays.py
+++ leetcode/median_of_two_sorted_arrays.py
@@ -1,11 +1,31 @@
-import numpy as np
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        n_large = 0
+        n_small = 0
+        v_smallest_in_large = None
+        v_largest_in_small = None
 
-m = np.array([2,3,4,5,2,3,4,2])
-n = np.array([2,3,4])
+        if len(nums1) > len(nums2):
+            list_long = nums1
+            list_short = nums2
+        else:
+            list_long = nums2
+            list_short = nums1
+
+        
 
 
 
+a1 = [1., 2., 3., 4., 5.]
+a2 = [4., 6., 7., 9.]
 
+obj = Solution()
+obj.findMedianSortedArrays(a1, a2)
 
 
 
