d8e16e76dc440f65b748201ad42ce5d7a372f403
Yuanhao Yu <yhyu.mail@gmail.com>
2018-06-20 17:08:55 -0400

    regular expression matching: formulate the idea

--- leetcode/regular_expression_matching.py
+++ leetcode/regular_expression_matching.py
@@ -1,9 +1,32 @@
 class Solution:
+    def matchFollowing(self, s, p, ix):
+        if len(s) - 1 < ix or len(p) - 1 < ix:
+            return False
+        if p[ix] != '*' and p[ix] != '.':
+            if p[ix] == s[ix]:
+                return True
+            else:
+                return False
+        if p[ix] == '.':
+            print(" . ")
+        if p[ix] == '*':
+            print(" * ")
+
+
+
+
     def isMatch(self, s, p):
         """
         :type s: str
         :type p: str
         :rtype: bool
         """
+        if p[0] == '*':
+            return -1
+        self.matchFollowing(s, p, 9)
 
+
+s = "mississippi"
+p = "mis*is*p*."
 a = Solution()
+a.isMatch(s, p)
