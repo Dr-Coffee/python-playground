29575c24331a7549b661f59c5ead8a56f760c750
Yuanhao Yu <yhyu.mail@gmail.com>
2018-07-14 20:48:23 -0400

    Update house_robber.py

--- leetcode/house_robber.py
+++ leetcode/house_robber.py
@@ -4,6 +4,7 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
+        
         last, now = 0, 0
         for i in nums: 
             last, now = now, max(last + i, now)
