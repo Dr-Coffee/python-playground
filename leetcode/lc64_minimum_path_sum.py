51f79bdd5378ed94e4f37cd8692700c89e113f08
Yuanhao Yu <yhyu.mail@gmail.com>
2018-07-25 17:34:06 -0400

    lc64

--- leetcode/lc64_minimum_path_sum.py
+++ leetcode/lc64_minimum_path_sum.py
@@ -4,3 +4,28 @@ class Solution:
         :type grid: List[List[int]]
         :rtype: int
         """
+        n_row = len(grid)
+        n_col = len(grid[0])
+
+        result = [[0 for j in range(n_col)] for i in range(n_row)]
+        for i in range(n_row):
+            for j in range(n_col):
+                if i == 0 and j == 0:
+                    result[i][j] = grid[i][j]
+                elif i == 0:
+                    result[i][j] = grid[i][j] + result[i][j-1]
+                elif j == 0:
+                    result[i][j] = grid[i][j] + result[i-1][j]
+                else:
+                    result[i][j] = grid[i][j] + min(result[i-1][j], result[i][j-1])
+
+        return result[n_row-1][n_col-1]
+
+
+grid = [
+  [1,3,1],
+  [1,5,1],
+  [4,2,1]
+]
+obj = Solution()
+print(obj.minPathSum(grid))
