02cbf1700a927fdfb429f880c064cf822899c45b
Yuanhao Yu <yhyu.mail@gmail.com>
2018-07-26 10:19:27 -0400

    lc847: shortest path visiting all nodes

---
+++ leetcode/lc847_shortest_path_visiting_all_nodes.py
@@ -0,0 +1,11 @@
+class Solution:
+    def shortestPathLength(self, graph):
+        """
+        :type graph: List[List[int]]
+        :rtype: int
+        """
+        print(graph)
+
+graph = [[1,2,3],[0],[0],[0]]
+obj = Solution()
+obj.shortestPathLength(graph)
