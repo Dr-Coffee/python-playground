6761beb7e79ef945183dfd2b95f25da600f7483a
Yuanhao Yu <yhyu.mail@gmail.com>
2018-07-30 11:11:30 -0400

    lc847

--- leetcode/lc847_shortest_path_visiting_all_nodes.py
+++ leetcode/lc847_shortest_path_visiting_all_nodes.py
@@ -1,4 +1,16 @@
 class Solution:
+    parent = dict()
+    rank = dict()
+
+    def make_set(self, input):
+        self.parent.update({input:input})
+
+    def find_set(self, input):
+        pass
+
+    def union(self, input):
+        pass
+
     def shortestPathLength(self, graph):
         """
         :type graph: List[List[int]]
